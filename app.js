4b8e880b07ac76a39c7b6f6f86e8928c5aa6a579
Mahnaz <md.jazayeri.sev@gmail.com>
2022-05-29 17:18:24 +0200

    .

--- app.js
+++ app.js
@@ -19,26 +19,3 @@ const tick = () => {
 setInterval(tick, 1000);
 
 
-
-let loader = document.querySelector('#loader');
-
-function loadNow(opacity) {
-    if (opacity <= 0) {
-        displayContent();
-    } else {
-        loader.style.opacity = opacity;
-        window.setTimeout(function() {
-            loadNow(opacity - 0.04);
-        }, 50);
-    }
-}
-
-function displayContent() {
-    loader.style.display = 'none';
-    document.getElementById('content').style.display = 'block';
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-    loader = document.getElementById('loader');
-    loadNow(1);
-});
